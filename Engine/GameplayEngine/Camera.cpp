55c0a8ced00ee115e2b096e783609752d97c939e
Ainur Gazizov <ainurdada@gmail.com>
2024-03-04 13:01:24 +0300

    fix aspect ratio

--- Engine/GameplayEngine/Camera.cpp
+++ Engine/GameplayEngine/Camera.cpp
@@ -12,30 +12,24 @@ Camera::Camera() : GameObject()
 	);
 }
 
-
-
 const SMath::Matrix& Camera::ViewMatrix()
 {
-	// Инициализация матрицы вида
-	XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);    // Куда смотрим
-
-	XMVECTOR Eye = XMVectorSet(0.0f, 5.0f, -10.0f, 0.0f);  // Откуда смотрим
-
-	XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);    // Направление верха
-
 	m_viewMatrix = XMMatrixLookAtLH(
 		transform.position(),
 		transform.position() + transform.Forward(),
 		transform.Up()
 	);
-
-	//m_viewMatrix = XMMatrixLookAtLH(Eye, At, Up);
-
 	return m_viewMatrix;
 }
 
+#include "../../App/Game.h"
+
 const SMath::Matrix& Camera::ProjectionMatrix()
 {
-	m_projectionMatrix = XMMatrixPerspectiveFovLH(XM_PIDIV4, aspectRation, minClipDistance, maxClipDistance);
+	m_projectionMatrix = XMMatrixPerspectiveFovLH(
+		XM_PIDIV4, 
+		Game::instance->window.ClientWidth / Game::instance->window.ClientHeight,
+		minClipDistance, 
+		maxClipDistance);
 	return m_projectionMatrix;
 }
